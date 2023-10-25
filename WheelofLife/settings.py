05abe30c06a961089e6fdf1bde8e9d528558ddc1
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2023-10-25 06:17:55 -0500

    Update settings.py

--- WheelofLife/settings.py
+++ WheelofLife/settings.py
@@ -176,7 +176,10 @@ MEDIA_ROOT = "user/static/img/media_users"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[BASE_DIR / "static",]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
