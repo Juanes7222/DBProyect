4bed89c13c4136bce3d4604d7138f791a1044fc7
Juanes7222 <juan.cardona13@utp.edu.co>
2023-10-28 17:35:54 -0500

    update

--- constructor.sh
+++
@@ -1,9 +0,0 @@
-
-python -m venv venv
-if [ -z "$VIRTUAL_ENV" ]; then
-    source ./venv/bin/activate
-fi
-pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
